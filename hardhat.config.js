af153bfa450eac9102885c2f7aa6cae8ea7347b3
Christopher Pearson <kermit4@gmail.com>
2021-04-22 16:13:25 +0000

    add solc version 0.4.25 to hardhard config

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
             enabled: true,
             runs: 200
           }
+        },
+      },{
+        version: '0.4.25',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
         }
       },{
         version: '0.7.6',
