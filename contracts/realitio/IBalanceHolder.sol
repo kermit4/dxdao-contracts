6676e8a03a4874c67817347bc7a8b03b5f14aa7d
Christopher Pearson <kermit4@gmail.com>
2021-04-24 21:23:34 +0000

    cruft i thought i deleted

--- contracts/realitio/IBalanceHolder.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.25;
-
-interface IBalanceHolder {
-  function withdraw (  ) external;
-  function balanceOf ( address ) external view returns ( uint256 );
-}
--- contracts/realitio/IOwned.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.25;
-
-interface IOwned {
-  function owner (  ) external view returns ( address );
-  function transferOwnership ( address newOwner ) external;
-}
