c93edb6abd888e1b5a764899e04030ba11902db5
Christopher Pearson <cpearson@lnlhb.com>
2021-04-27 04:17:27 +0000

    js-beautify

--- test/OMNGuild.js
+++ test/OMNGuild.js
@@ -3,144 +3,174 @@ const constants = require("./helpers/constants");
 const OMNGuild = artifacts.require("OMNGuild");
 const Realitio = artifacts.require("Realitio");
 const ActionMock = artifacts.require("ActionMock");
-const { fixSignature } = require("./helpers/sign");
 const {
-  BN,
-  expectEvent,
-  expectRevert,
-  balance,
-  send,
-  ether,
-  time
+    fixSignature
+} = require("./helpers/sign");
+const {
+    BN,
+    expectEvent,
+    expectRevert,
+    balance,
+    send,
+    ether,
+    time
 } = require("@openzeppelin/test-helpers");
 const {
-  createAndSetupGuildToken,
+    createAndSetupGuildToken,
 } = require("./helpers/guild");
 
 require("chai").should();
 
-contract("OMNGuild", function (accounts) {
-  
-  const ZERO = new BN("0");
-  const TIMELOCK = new BN("60");
-  const VOTE_GAS = new BN("50000"); // 50k
-  const MAX_GAS_PRICE = new BN("8000000000"); // 8 gwei
+contract("OMNGuild", function(accounts) {
 
-  let actionMock,
-    guildToken,
-    omnGuild,
-	realitio,
-    tokenVault,
-    callData,
-    genericCallData,
-    questionId,
-    genericProposal;
-    
-  beforeEach(async function () {
-    guildToken = await createAndSetupGuildToken(
-      accounts.slice(0, 5), [0, 50, 100, 150, 200]
-    );
-    omnGuild = await OMNGuild.new();
-    realitio = await Realitio.new();
-	questionId = (await realitio.askQuestion(0,"test question?",omnGuild.address,30,0,1)).receipt.logs[0].args.question_id;
-   
-await realitio.submitAnswer(questionId, web3.utils.asciiToHex("filler"), 0,{value:1});
+    const ZERO = new BN("0");
+    const TIMELOCK = new BN("60");
+    const VOTE_GAS = new BN("50000"); // 50k
+    const MAX_GAS_PRICE = new BN("8000000000"); // 8 gwei
 
-    actionMock = await ActionMock.new();
+    let actionMock,
+        guildToken,
+        omnGuild,
+        realitio,
+        tokenVault,
+        callData,
+        genericCallData,
+        questionId,
+        genericProposal;
 
-    await omnGuild.initialize(
-      guildToken.address, 30, 130000, 40, 0, VOTE_GAS, MAX_GAS_PRICE, TIMELOCK, 99999,  realitio.address
-    );
-	omnGuild.setOMNGuildConfig(1000,realitio.address,1000,1000);
+    beforeEach(async function() {
+        guildToken = await createAndSetupGuildToken(
+            accounts.slice(0, 5), [0, 50, 100, 150, 200]
+        );
+        omnGuild = await OMNGuild.new();
+        realitio = await Realitio.new();
+        questionId = (await realitio.askQuestion(0, "test question?", omnGuild.address, 30, 0, 1)).receipt.logs[0].args.question_id;
 
-    tokenVault = await omnGuild.tokenVault();
+        await realitio.submitAnswer(questionId, web3.utils.asciiToHex("filler"), 0, {
+            value: 1
+        });
 
-    await guildToken.approve(tokenVault, 50, { from: accounts[1] });
-    await guildToken.approve(tokenVault, 100, { from: accounts[2] });
-    await guildToken.approve(tokenVault, 150, { from: accounts[3] });
-    await guildToken.approve(tokenVault, 200, { from: accounts[4] });
+        actionMock = await ActionMock.new();
 
-    await omnGuild.lockTokens(50, { from: accounts[1] });
-    await omnGuild.lockTokens(100, { from: accounts[2] });
-    await omnGuild.lockTokens(150, { from: accounts[3] });
-    await omnGuild.lockTokens(200, { from: accounts[4] });
-    
-    tokenVault = await omnGuild.tokenVault();
+        await omnGuild.initialize(
+            guildToken.address, 30, 130000, 40, 0, VOTE_GAS, MAX_GAS_PRICE, TIMELOCK, 99999, realitio.address
+        );
+        omnGuild.setOMNGuildConfig(1000, realitio.address, 1000, 1000);
 
-  });
+        tokenVault = await omnGuild.tokenVault();
 
-  describe("OMNGuild", function () {
+        await guildToken.approve(tokenVault, 50, {
+            from: accounts[1]
+        });
+        await guildToken.approve(tokenVault, 100, {
+            from: accounts[2]
+        });
+        await guildToken.approve(tokenVault, 150, {
+            from: accounts[3]
+        });
+        await guildToken.approve(tokenVault, 200, {
+            from: accounts[4]
+        });
 
-    it("vote on and execute a market validation proposal from the omn-guild", async function () {
-       const tx = await  omnGuild.createMarketValidationProposal(questionId);
+        await omnGuild.lockTokens(50, {
+            from: accounts[1]
+        });
+        await omnGuild.lockTokens(100, {
+            from: accounts[2]
+        });
+        await omnGuild.lockTokens(150, {
+            from: accounts[3]
+        });
+        await omnGuild.lockTokens(200, {
+            from: accounts[4]
+        });
 
-  const guildProposalId = tx.logs[0].args.proposalId;
-  
-  await expectRevert(
-        omnGuild.endProposal(guildProposalId),
-        "OMNGuild: Use endMarketValidationProposal to end proposals to validate market"
-      );
-      await expectRevert(
-        omnGuild.endMarketValidationProposal(questionId),
-        "OMNGuild: Market valid proposal hasnt ended yet"
-      );
-	const votes  = await omnGuild.methods['votesOf(address)'](accounts[4]); // overloaded function which is not supported by truffle
-     const txVote = await omnGuild.setVote(
-			 guildProposalId,
-			 votes,
-			 {from: accounts[4]});
-      
-	 expectEvent(txVote, "VoteAdded", { proposalId: guildProposalId });
-
-      await time.increase(time.duration.seconds(100000));
-      
-      if (constants.ARC_GAS_PRICE > 1)
-        expect(txVote.receipt.gasUsed).to.be.below(80000);
-
-      await expectRevert(
-        omnGuild.endProposal(guildProposalId),
-        "OMNGuild: Use endMarketValidationProposal to end proposals to validate market"
-      );
-      const receipt = await omnGuild.endMarketValidationProposal(questionId);
- expectEvent(receipt, "ProposalExecuted", { proposalId: guildProposalId });
-      await expectRevert(
-        omnGuild.endMarketValidationProposal(questionId),
-        "OMNGuild: Market valid proposal already executed"
-      );
-      const proposalInfo = await omnGuild.getProposal(guildProposalId);
-      assert.equal(proposalInfo.state, constants.GuildProposalState.Executed);
-      assert.equal(proposalInfo.to[0], realitio.address);
-      assert.equal(proposalInfo.value[0], 0);
-      await time.increase(time.duration.seconds(31));
-    });
-    it("test proposal failed/ended", async function () {
-       const tx = await  omnGuild.createMarketValidationProposal(questionId);
-
-  const guildProposalId = tx.logs[0].args.proposalId;
-  
-	const votes  = await omnGuild.methods['votesOf(address)'](accounts[4]); // overloaded function which is not supported by truffle
-     const txVote = await omnGuild.setVote(
-			 guildProposalId,
-			 votes,
-			 {from: accounts[4]});
-      await time.increase(time.duration.seconds(200000));
-      const receipt = await omnGuild.endMarketValidationProposal(questionId);
- expectEvent(receipt, "ProposalEnded", { proposalId: guildProposalId });
-      const proposalInfo = await omnGuild.getProposal(guildProposalId);
-      assert.equal(proposalInfo.state, constants.GuildProposalState.Failed);
+        tokenVault = await omnGuild.tokenVault();
 
     });
-    it("test proposal rejected", async function () {
-       const tx = await  omnGuild.createMarketValidationProposal(questionId);
 
-  const guildProposalId = tx.logs[0].args.proposalId;
-  
-      await time.increase(time.duration.seconds(100000));
-      const receipt = await omnGuild.endMarketValidationProposal(questionId);
- expectEvent(receipt, "ProposalRejected", { proposalId: guildProposalId });
-      const proposalInfo = await omnGuild.getProposal(guildProposalId);
-      assert.equal(proposalInfo.state, constants.GuildProposalState.Rejected);
+    describe("OMNGuild", function() {
 
+        it("vote on and execute a market validation proposal from the omn-guild", async function() {
+            const tx = await omnGuild.createMarketValidationProposal(questionId);
+
+            const guildProposalId = tx.logs[0].args.proposalId;
+
+            await expectRevert(
+                omnGuild.endProposal(guildProposalId),
+                "OMNGuild: Use endMarketValidationProposal to end proposals to validate market"
+            );
+            await expectRevert(
+                omnGuild.endMarketValidationProposal(questionId),
+                "OMNGuild: Market valid proposal hasnt ended yet"
+            );
+            const votes = await omnGuild.methods['votesOf(address)'](accounts[4]); // overloaded function which is not supported by truffle
+            const txVote = await omnGuild.setVote(
+                guildProposalId,
+                votes, {
+                    from: accounts[4]
+                });
+
+            expectEvent(txVote, "VoteAdded", {
+                proposalId: guildProposalId
+            });
+
+            await time.increase(time.duration.seconds(100000));
+
+            if (constants.ARC_GAS_PRICE > 1)
+                expect(txVote.receipt.gasUsed).to.be.below(80000);
+
+            await expectRevert(
+                omnGuild.endProposal(guildProposalId),
+                "OMNGuild: Use endMarketValidationProposal to end proposals to validate market"
+            );
+            const receipt = await omnGuild.endMarketValidationProposal(questionId);
+            expectEvent(receipt, "ProposalExecuted", {
+                proposalId: guildProposalId
+            });
+            await expectRevert(
+                omnGuild.endMarketValidationProposal(questionId),
+                "OMNGuild: Market valid proposal already executed"
+            );
+            const proposalInfo = await omnGuild.getProposal(guildProposalId);
+            assert.equal(proposalInfo.state, constants.GuildProposalState.Executed);
+            assert.equal(proposalInfo.to[0], realitio.address);
+            assert.equal(proposalInfo.value[0], 0);
+            await time.increase(time.duration.seconds(31));
+        });
+        it("test proposal failed/ended", async function() {
+            const tx = await omnGuild.createMarketValidationProposal(questionId);
+
+            const guildProposalId = tx.logs[0].args.proposalId;
+
+            const votes = await omnGuild.methods['votesOf(address)'](accounts[4]); // overloaded function which is not supported by truffle
+            const txVote = await omnGuild.setVote(
+                guildProposalId,
+                votes, {
+                    from: accounts[4]
+                });
+            await time.increase(time.duration.seconds(200000));
+            const receipt = await omnGuild.endMarketValidationProposal(questionId);
+            expectEvent(receipt, "ProposalEnded", {
+                proposalId: guildProposalId
+            });
+            const proposalInfo = await omnGuild.getProposal(guildProposalId);
+            assert.equal(proposalInfo.state, constants.GuildProposalState.Failed);
+
+        });
+        it("test proposal rejected", async function() {
+            const tx = await omnGuild.createMarketValidationProposal(questionId);
+
+            const guildProposalId = tx.logs[0].args.proposalId;
+
+            await time.increase(time.duration.seconds(100000));
+            const receipt = await omnGuild.endMarketValidationProposal(questionId);
+            expectEvent(receipt, "ProposalRejected", {
+                proposalId: guildProposalId
+            });
+            const proposalInfo = await omnGuild.getProposal(guildProposalId);
+            assert.equal(proposalInfo.state, constants.GuildProposalState.Rejected);
+
+        });
     });
-  });
 });
